177434d455d66b0158a36c778be317bac0940f4d
tiniuclx <tiniuc.alexandru@gmail.com>
2017-05-05 10:25:30 +0000

    Added comments concerning the serialisation process

--- main.cpp
+++ main.cpp
@@ -20,38 +20,41 @@ struct SerialPins{
     int isSending;
     int hasRead;
 };
-
+// Structure containing the data passed to each thread
 struct thread_data{
     ThreadSafeQueue *send_queue;
     SerialReceiver *receiver;
     SerialPins *serial_pins;
 };
-
-
-
+// Stub to implement simulated serial.
+// Makes it easy to use physical pins: simply modify this function
 int readPin(const int &pin){
     return pin;
 }
-
+// Stub to implement simulated serial.
+// Makes it easy to use physical pins: simply modify this function
 void writePin(int &pin, int value){
     pin = value;
 }
-
+// For debugging purposes: prints out the values of a vector<bool>
+// to qDebug()
 void printBool(const std::vector<bool> array){
     QDebug deb = qDebug();
     for(uint i=0;i<array.size();i++)
         deb<<(int) array[i];
 }
 
-void* sendThread(void* information)
-{
+void* sendThread(void* information) {
+    // Cast the void pointer to a pointer of the expected type
     thread_data *data = (thread_data*) information;
-
     while(1){
+        // If the send queue is empty
         if(data->send_queue->size() != 0){
+            // Pop out a message
             std::vector<bool> bitfield = data->send_queue->front();
             data->send_queue->pop();
-            for(int i = 0; i<bitfield.size(); i++){
+            // Send the message over serial
+            for(unsigned int i = 0; i<bitfield.size(); i++){
                 writePin(data->serial_pins->data, bitfield[i]);
                 writePin(data->serial_pins->isSending, TRUE);
                 while(!readPin(data->serial_pins->hasRead))
@@ -66,20 +69,20 @@ void* sendThread(void* information)
     pthread_exit(NULL);
 }
 
-void* receiveThread(void* information)
-{
+void* receiveThread(void* information) {
     thread_data *data = (thread_data*) information;    
     std::vector<bool> bitfield;
-
     while(1){
+        // When remote stops sending, make read
+        // confirmation flag LOW
         while(readPin(data->serial_pins->isSending))
             ;
         writePin(data->serial_pins->hasRead, FALSE);
-
         while(!readPin(data->serial_pins->isSending))
             ;
         bitfield.push_back(readPin(data->serial_pins->data));
         // If the message has the right size
+        // Magic number! Consider removing.
         if(bitfield.size() == 22){
             // Tells Viewer to draw the point, serialised as bitfield.
             data->receiver->decoder(bitfield);
@@ -103,24 +106,23 @@ int main(int argc, char *argv[])
     // the two windows are initialised in the main loop    
     SendWindow s;
     ReceiveWindow r;
-
     s.show();
     r.show();
-
+    // Data shared between threads
     thread_data data;
     ThreadSafeQueue send_queue;
-
+    // Serialization classes
     SerialReceiver receiver(r.viewer);
     SerialSender sender(s.canvas, &receiver,&send_queue);
-
+    // Sets up thread data
     data.send_queue = &send_queue;
     data.receiver = &receiver;
-
+    // Simulated pins: initially start LOW to prevent
+    // initialisation errors
     SerialPins serial_pins;
     serial_pins.data = 0;
     serial_pins.hasRead = 0;
     serial_pins.isSending = 0;
-
     data.serial_pins = &serial_pins;
 
     // starting worker thread(s)
@@ -131,22 +133,17 @@ int main(int argc, char *argv[])
         qDebug() << "Unable to start send thread.";
         exit(1);
     }
-
     rc = pthread_create(&receive_thread, NULL, receiveThread, (void*) &data);
     if (rc) {
         qDebug() << "Unable to start receive thread.";
         exit(1);
     }
-
-
     // start window event loop
     qDebug() << "Starting event loop...";
     int ret = a.exec();
     qDebug() << "Event loop stopped.";
-
     // cleanup pthreads
     pthread_exit(NULL);
-
     // exit
     return ret;
 }
